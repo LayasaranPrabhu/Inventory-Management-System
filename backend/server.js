34b0f0119f19a05737e380612aae947a320f4d33
Layasaran <pvlayasaran@gmail.com>
2025-04-17 18:49:02 +0530

    init

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: 'http://localhost:5173/',
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
